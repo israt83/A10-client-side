a5e950bea9fb1b9ef7515f5660368b0be1424300
Israt Jahan <Israt Jahan>
2024-06-21 20:52:29 +0600

    update file

--- src/Firebase/firebaseConfig.js
+++ src/Firebase/firebaseConfig.js
@@ -1,6 +1,25 @@
 
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAuth } from "firebase/auth";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
 
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyC_LGQY2o6op9yqaAX_dWYoUz_3dTLfSg0",
+//   authDomain: "tourism-website-e8f36.firebaseapp.com",
+//   projectId: "tourism-website-e8f36",
+//   storageBucket: "tourism-website-e8f36.appspot.com",
+//   messagingSenderId: "971816593279",
+//   appId: "1:971816593279:web:0cabe462102ca4f86e1c4a"
+// };
 
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const auth = getAuth(app);
+
+// export default auth;
 
 
 // Import the functions you need from the SDKs you need
@@ -11,12 +30,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC_LGQY2o6op9yqaAX_dWYoUz_3dTLfSg0",
-  authDomain: "tourism-website-e8f36.firebaseapp.com",
-  projectId: "tourism-website-e8f36",
-  storageBucket: "tourism-website-e8f36.appspot.com",
-  messagingSenderId: "971816593279",
-  appId: "1:971816593279:web:0cabe462102ca4f86e1c4a"
+  apiKey: "AIzaSyDjnj8lqdY4wvSq7kiw1SjgEy8kKVXLBuM",
+  authDomain: "tourism-websites.firebaseapp.com",
+  projectId: "tourism-websites",
+  storageBucket: "tourism-websites.appspot.com",
+  messagingSenderId: "573631836618",
+  appId: "1:573631836618:web:a4b9b65db1ea12a2005f33"
 };
 
 // Initialize Firebase
@@ -24,5 +43,3 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
 export default auth;
-
-
